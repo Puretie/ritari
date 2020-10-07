81ad17aba17229f4501e3808d209006b616960b4
Tim <dabon696@yahoo.com>
2020-10-07 14:05:14 -0400

    Ifini-list

---
+++ lib/main.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<int> items = List.generate(20, (i) => i);
+  ScrollController _scrollController = new ScrollController();
+  bool isRequesting = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _getMoreData();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  _getMoreData() async {
+    if (!isRequesting) {
+      setState(() => isRequesting = true);
+      List<int> moreStuff = await fRequest(items.length, items.length + 10);
+      setState(() {
+        items.addAll(moreStuff);
+        isRequesting = false;
+      });
+    }
+  }
+
+  Widget progressIndicator() {
+    return new Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Center(
+        child: Opacity(
+          opacity: isRequesting ? 1.0 : 0.0,
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Some Text"),
+      ),
+      body: Stack(
+        children: [
+          ListView.builder(
+            itemCount: items.length + 1,
+            itemBuilder: (context, index) {
+              if (index == items.length) {
+                return progressIndicator();
+              } else {
+                return ListTile(
+                  onTap: () {
+                    //note menu
+                  },
+                  title: Text("Number $index"),
+                );
+              }
+            },
+            controller: _scrollController,
+          ),
+          Align(
+              alignment: Alignment.bottomRight,
+              child: FloatingActionButton(
+                backgroundColor: Colors.teal.shade400,
+                child: Icon(Icons.add),
+                onPressed: () {
+                  //createnote
+                },
+              ))
+        ],
+      ),
+    );
+  }
+
+  Future<List<int>> fRequest(int from, int to) async {
+    return Future.delayed(Duration(seconds: 2), () {
+      return List.generate(to - from, (i) => i + from);
+    });
+  }
+}
